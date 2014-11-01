6f609a783a924150968fc19c2ab43bc74b39fe74
João Vieira <felixvieira@gmail.com>
2014-11-01 03:06:06 +0000

    Starting Demo2

---
+++ Assets/Scripts/2DDynamicLights/SpriteIlluminationScript.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteIlluminationScript : MonoBehaviour {
+
+
+    public class LightSourceInfo {
+        public Color lightcolor { get; set; }
+        public Color alpha { get; set; }
+
+    }
+
+    private SpriteRenderer _spriteRenderer;
+    private Color OriginalColor;
+    public float InitialIllumination; //Value: 0 To be invisible without light
+    private Color newColor;
+    public float LerpSpeed = 5;
+    private bool canReset;
+    //public Color color;
+    // Use this for initialization
+    void Start() {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+
+        OriginalColor = _spriteRenderer.color;
+
+        _spriteRenderer.color = new Color(_spriteRenderer.color.r, _spriteRenderer.color.g, _spriteRenderer.color.b, InitialIllumination);
+        newColor = _spriteRenderer.color;
+        //_spriteRenderer.material.SetColor("_Color", color);
+    }
+
+    // Update is called once per frame
+    void Update() {
+     _spriteRenderer.color = Color.Lerp(_spriteRenderer.color, newColor, Time.deltaTime*LerpSpeed);
+    }
+
+    void OnCollisionEnter2D(Collision2D col) {
+        print(col.collider.name);
+    }
+
+    public void SetIllumination(LightSourceInfo info) {
+        //current_intensity = intensity;
+
+        //float new_alpha = intensity / 255f;
+        CancelInvoke("ResetNewColor");
+        newColor = OriginalColor * info.lightcolor;
+        newColor.a = map(info.alpha.a, 0, info.lightcolor.a, 0, 1);
+        Invoke("ResetNewColor", .3f);
+        
+        //_spriteRenderer.color = newColor;
+        //Color.Lerp(_spriteRenderer.color, newColor, Time.deltaTime);
+        //_spriteRenderer.color.a
+    }
+
+
+    void ResetNewColor() {
+            newColor.a = 0;            
+    }
+
+    float map(float s, float a1, float a2, float b1, float b2) {
+        return b1 + (s - a1) * (b2 - b1) / (a2 - a1);
+    }
+}
---
+++ Assets/Scripts/JointScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JointScript : MonoBehaviour {
+
+    Transform attached;
+
+	// Use this for initialization
+	void Start () {
+        attached = GetComponent<SpringJoint2D>().connectedBody.gameObject.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.DrawLine(transform.position, attached.position, Color.green);
+	}
+
+}
